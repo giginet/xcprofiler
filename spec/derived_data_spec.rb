5245b466342e1d0de0093321ace6890ebbbde56a
giginet <giginet.net@gmail.com>
2016-12-21 13:50:03 +0900

    Add derived_data specs

--- spec/derived_data_spec.rb
+++ spec/derived_data_spec.rb
@@ -9,18 +9,50 @@ describe DerivedData do
   end
 
   describe '#by_product_name' do
-    context "with existing product name" do
+    context 'with valid log' do
       it 'returns DerivedData' do
         derived_data = DerivedData.by_product_name('MyApp')
         expect(derived_data).to_not be_nil
       end
+
+      context 'with not existing log' do
+        it 'raises' do
+          expect {
+            DerivedData.by_product_name('NotExist')
+          }.to raise_error('Any matching derived data are not found'.red)
+        end
+      end
+    end
+  end
+
+  context 'with valid log' do
+    let(:derived_data) { DerivedData.by_product_name('MyApp') }
+
+    describe '#flag_enabled?' do
+      it 'returns true' do
+        expect(derived_data.flag_enabled?).to be_truthy
+      end
     end
 
-    context 'with not existing product name' do
-      it 'raises' do
-        expect {
-          DerivedData.by_product_name('Invalid')
-        }.to raise_error('Any matching derived data are not found'.red)
+    describe '#executions' do
+      it 'returns array' do
+        expect(derived_data.executions).not_to be_empty
+      end
+    end
+  end
+
+  context 'with invalid log' do
+    let(:derived_data) { DerivedData.by_product_name('Invalid') }
+
+    describe '#flag_enabled?' do
+      it 'returns false' do
+        expect(derived_data.flag_enabled?).to be_falsey
+      end
+    end
+
+    describe '#executions' do
+      it 'returns empty array' do
+        expect(derived_data.executions).to be_empty
       end
     end
   end
