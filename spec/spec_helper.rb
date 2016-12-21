03247f989c973ceb570d2b9db19f26ba1c67b209
giginet <giginet.net@gmail.com>
2016-12-21 22:57:12 +0900

    Use wear!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'xctracker'
 require 'coveralls'
-Coveralls.wear_merged!
-SimpleCov.merge_timeout 3600
+Coveralls.wear!
+
+require 'xctracker'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -50,7 +50,7 @@ RSpec.configure do |config|
   # compatibility in RSpec 3). It causes shared context metadata to be
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
-  config.shared_context_metadata_behavior = :apply_to_host_groups
+  #config.shared_context_metadata_behavior = :apply_to_host_groups
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
