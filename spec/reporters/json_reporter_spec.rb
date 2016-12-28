4600fad73f48098b9efb623e7fe3e9f80825ccb2
giginet <giginet.net@gmail.com>
2016-12-28 10:59:07 +0900

    Add specs for JSONReporter

---
+++ spec/reporters/json_reporter_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+include Xcprofiler
+
+describe JSONReporter do
+  let(:profiler) do
+    Profiler.by_product_name('MyApp').tap do |profiler|
+      profiler.reporters = [reporter]
+    end
+  end
+
+  before do
+    fixture_root = File.absolute_path(File.join(__FILE__, '../../fixtures'))
+    allow(DerivedData).to receive(:derived_data_root).and_return(fixture_root)
+  end
+
+  context 'with output path' do
+    let(:dir) { Dir.mktmpdir }
+    let(:output_path) { File.join(dir, 'result.json') }
+    let(:reporter) { JSONReporter.new(output_path: output_path) }
+
+    it 'exports json' do
+      profiler.report!
+      expect(File.exist?(output_path)).to be_truthy
+    end
+  end
+end
